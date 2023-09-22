4c204c1dd990057124c880cb832d62ead50460ed
manish <maneeshbalami@gmail.com>
2023-09-22 18:31:39 +0545

    added upper navbar with nightmode

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const nav = document.getElementById("navbar");
+const toggleSwitch = document.getElementById("toggleswitch");
 
 window.onscroll = function () { scrollShowNavbar() };
+toggleSwitch.addEventListener('change', nightMode);
 
 function scrollShowNavbar() {
+  const nav = document.getElementById("secondNav");
+
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
     nav.style.position = "fixed";
+    nav.style.marginTop = "-40px";
   } else {
     nav.style.position = "static";
+    nav.style.marginTop = "0";
   }
+}
+
+function nightMode(){
+  document.body.classList.toggle("nightmode");
 }
