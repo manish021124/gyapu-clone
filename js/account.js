ce52815575e0f0246c99d5ea6e29baf0a99d1c49
manish <maneeshbalami@gmail.com>
2023-10-21 17:24:09 +0545

    updated account section

--- js/account.js
+++ js/account.js
@@ -4,10 +4,19 @@ const loginPwdInput = document.getElementById("login-pwd");
 const registerPwdImage = document.getElementById("register-pwd-visibility-image");
 const registerPwdConfirmImage = document.getElementById("register-pwd-confirm-visibility-image");
 const loginPwdImage = document.getElementById("login-pwd-visibility-image");
+const accordionRegisterPwdInput = document.getElementById("accordion-register-pwd");
+const accordionRegisterPwdConfirmInput = document.getElementById("accordion-register-confirm-pwd");
+const accordionLoginPwdInput = document.getElementById("accordion-login-pwd");
+const accordionRegisterPwdImage = document.getElementById("accordion-register-pwd-visibility-image");
+const accordionRegisterPwdConfirmImage = document.getElementById("accordion-register-pwd-confirm-visibility-image");
+const accordionLoginPwdImage = document.getElementById("accordion-login-pwd-visibility-image");
 
 registerPwdImage.addEventListener("click", function () { togglePasswordVisibility(registerPwdInput, registerPwdImage) });
 registerPwdConfirmImage.addEventListener("click", function () { togglePasswordVisibility(registerPwdConfirmInput, registerPwdConfirmImage) });
 loginPwdImage.addEventListener("click", function () { togglePasswordVisibility(loginPwdInput, loginPwdImage) });
+accordionRegisterPwdImage.addEventListener("click", function () { togglePasswordVisibility(accordionRegisterPwdInput, accordionRegisterPwdImage) });
+accordionRegisterPwdConfirmImage.addEventListener("click", function () { togglePasswordVisibility(accordionRegisterPwdConfirmInput, accordionRegisterPwdConfirmImage) });
+accordionLoginPwdImage.addEventListener("click", function () { togglePasswordVisibility(accordionLoginPwdInput, accordionLoginPwdImage) });
 
 function togglePasswordVisibility(input, image) {
   if (input.type === "password") {
